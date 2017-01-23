56a8b5d413bce7d48426c864d652e93b8e44cc19
Asaf Menahem <asimen1@gmail.com>
2017-01-23 23:57:18 +0200

    Bump to version 1.0.3

--- src/extension/js/devtool/about_popup.jsx
+++ src/extension/js/devtool/about_popup.jsx
@@ -7,7 +7,7 @@ let AboutPopup = React.createClass({
     render: function() {
         return (
             <div id = 'aboutPopup'>
-                <h2>Restyler (version 1.0.2)</h2>
+                <h2>Restyler (version 1.0.3)</h2>
                 <div id = 'aboutText'>Created by <b>Asaf Menahem</b>, with the help of <b>Nir Mizrahi</b> & <b>Orly Boojor</b> (A.K.A The Dream Team).
                 This extension is <a href="https://github.com/asimen1/restyler" target="_blank" className="link">open source</a>, feel free to contribute :)</div>
             </div>
